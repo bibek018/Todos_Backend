640d62a2e5f08296cfa04e348867a39e73103346
Bibek Ojha <ojhabibek505@gmail.com>
2026-09-14 22:11:00 +0530

    fixed some bugs

--- controllers/controller.auth.google.js
+++ controllers/controller.auth.google.js
@@ -2,6 +2,8 @@ import { User } from "../model/User.js";
 import { generateAccessToken, generateRefreshToken } from "../utils/Token.js";
 import { catchAsync } from "../utils/catchAsync.js";
 import { AppError } from "../utils/AppError.js";
+import logger from "../utils/logger.js";
+
 export const googleAuthController = catchAsync(async (req, res, next) => {
   if (!req?.user?.email) {
     return next(new AppError("Google authentication failed", 400));
@@ -20,8 +22,35 @@ export const googleAuthController = catchAsync(async (req, res, next) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
-  logger.info("NODE_ENV:", process.env.NODE_ENV);
-  logger.info("CLIENT_ORIGIN:", process.env.CLIENT_ORIGIN);
+  logger.info("========== ENV CHECK STARTS ==========");
+  logger.info(`ACCESS_SECRET: ${JSON.stringify(process.env.ACCESS_SECRET)}`);
+  logger.info(`CLIENT_ORIGIN: ${JSON.stringify(process.env.CLIENT_ORIGIN)}`);
+  logger.info(
+    `CLOUDINARY_API_KEY: ${JSON.stringify(process.env.CLOUDINARY_API_KEY)}`,
+  );
+  logger.info(
+    `CLOUDINARY_API_SECRET: ${JSON.stringify(process.env.CLOUDINARY_API_SECRET)}`,
+  );
+  logger.info(
+    `CLOUDINARY_CLOUD_NAME: ${JSON.stringify(process.env.CLOUDINARY_CLOUD_NAME)}`,
+  );
+  logger.info(
+    `GITHUB_CLIENT_ID: ${JSON.stringify(process.env.GITHUB_CLIENT_ID)}`,
+  );
+  logger.info(
+    `GITHUB_CLIENT_SECRET: ${JSON.stringify(process.env.GITHUB_CLIENT_SECRET)}`,
+  );
+  logger.info(
+    `GOOGLE_CLIENT_ID: ${JSON.stringify(process.env.GOOGLE_CLIENT_ID)}`,
+  );
+  logger.info(
+    `GOOGLE_CLIENT_SECRET: ${JSON.stringify(process.env.GOOGLE_CLIENT_SECRET)}`,
+  );
+  logger.info(`MONGO_URI: ${JSON.stringify(process.env.MONGO_URI)}`);
+  logger.info(`NODE_ENV: ${JSON.stringify(process.env.NODE_ENV)}`);
+  logger.info(`REFRESH_SECRET: ${JSON.stringify(process.env.REFRESH_SECRET)}`);
+  logger.info(`SERVER_URL: ${JSON.stringify(process.env.SERVER_URL)}`);
+  logger.info("========== ENV CHECK ENDS ==========");
   user.refreshtoken = refreshtoken;
   await user.save();
 
